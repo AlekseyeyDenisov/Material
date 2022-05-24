480cfb8ddacdb8a5807324d6428c13f1c3984fa2
77den <77denisov@gmail.com>
2022-05-24 11:34:15 +0300

    домашнее задание к уроку №1 Сделал сингал активити

---
+++ app/src/main/java/ru/dw/material/view/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.dw.material.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.dw.material.R
+import ru.dw.material.view.picture.PictureOfTheDayFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance()).commit()
+        }
+    }
+}
---
+++ app/src/main/java/ru/dw/material/view/picture/PictureOfTheDayFragment.kt
@@ -0,0 +1,33 @@
+package ru.dw.material.view.picture
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.dw.material.R
+import ru.dw.material.databinding.FragmentPictureOfTheDayBinding
+
+
+class PictureOfTheDayFragment : Fragment() {
+
+    private var _binding: FragmentPictureOfTheDayBinding? = null
+    private val binding:FragmentPictureOfTheDayBinding
+    get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPictureOfTheDayBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = PictureOfTheDayFragment()
+    }
+}
